21c90d0cbbf3a12e430a2b2fb34fb9f8e7f8ee00
Shubham Shaw <42186894+supersonic181@users.noreply.github.com>
2021-03-20 02:01:21 +0530

    Even Better
    Added Error Handling and user-chosen final pdf name

--- Replacinator_v3.py
+++ Replacinator_v3.py
@@ -3,28 +3,33 @@ try:
 except:
     import sys
     import subprocess
-
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pyPDF2'])
 
+def valid_input_checker():
+    valid_input = False
+    while (valid_input == False):
+            input1 = input("Enter the starting and ending page number you want to copy from PDF1: ").split(' ')
+            if (len(input1) != 2) or (not input1[0].isnumeric()) or (not input1[1].isnumeric()):
+                print("Please enter 2 integers.")
+                valid_input = False
+            else:
+                valid_input = True
+    start, end = int(input1[0]),int(input1[1])
+    return start,end
+    
 destination = PyPDF2.PdfFileWriter()
 
 input("Please keep both Pdfs in the same folder where replacinator.py is...\nPress any key to proceed\n")
 
 first_pdf = input("Enter the first pdf name including .pdf: ")
 second_pdf = input("Enter the second pdf name including .pdf: ")
+final_pdf = input("Enter the final pdf name including .pdf: ")
 
-with open('Replacinator.pdf', 'wb') as newFile:
+with open(final_pdf, 'wb') as newFile:
     
     with open(first_pdf,'rb') as namePage:
         pdfReader_Name = PyPDF2.PdfFileReader(namePage)
-        valid_input = False
-        while (valid_input == False):
-            input1 = input("Enter the starting and ending page number you want to copy from PDF1: ").split(' ')
-            if (len(input1) != 2) or (not input1[0].isnumeric()) or (not input1[1].isnumeric()):
-                print("Please enter 2 integers.")
-            else:
-                valid_input = True
-        start, end = map(int, input1)
+        start,end = valid_input_checker()
         start = start - 1
         for i in range(start,end):
             name = pdfReader_Name.getPage(i)
@@ -34,18 +39,11 @@ with open('Replacinator.pdf', 'wb') as newFile:
     with open(second_pdf,'rb') as source:
         pdfReader_Source = PyPDF2.PdfFileReader(source)
         pageCount = pdfReader_Source.getNumPages()
-        valid_input = False
-        while (valid_input == False):
-            input1 = input("Enter the starting and ending page number you want to copy from PDF2: ").split(' ')
-            if (len(input1) != 2) or (not input1[0].isnumeric()) or (not input1[1].isnumeric()):
-                print("Please enter 2 integers.")
-            else:
-                valid_input = True
-        start, end = map(int, input1)
+        start,end = valid_input_checker()
         start = start - 1
         for i in range(start,end):
             ans = pdfReader_Source.getPage(i)
             destination.addPage(ans)
             destination.write(newFile)
             
-print("File Created Successfully!!!\nFile Name: Replacinator.pdf")
+print(f"File Created Successfully!!!\nFile Name: {final_pdf}")
